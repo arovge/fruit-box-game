3c30b262ed9e289ddf083991abda70da01b1945b
Austin <austinrovge@gmail.com>
2026-06-23 16:03:23 -0500

    fix typo

--- src/ui.rs
+++ src/ui.rs
@@ -42,7 +42,7 @@ pub fn button(text: impl Into<String>) -> impl Scene {
     }
 }
 
-// Parlay doesn't seem to support tabs in bevy 0.19:
+// Parley doesn't seem to support tabs in bevy 0.19:
 // https://github.com/linebender/parley/issues/302
 // https://github.com/bevyengine/bevy/issues/22955
 pub fn fix_tab(str: impl Into<String>) -> String {
@@ -53,7 +53,6 @@ pub fn fix_tab(str: impl Into<String>) -> String {
 
     for c in text.chars() {
         if c == '\t' {
-            // Calculate spaces needed to reach the next tab stop
             let spaces_needed = tab_width - (col % tab_width);
             result.push_str(&" ".repeat(spaces_needed));
             col += spaces_needed;
